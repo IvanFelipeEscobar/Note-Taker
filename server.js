1a2eb97c16c44bf19b4988535bb08df078a46697
Ivan Escobar <ivnescbr1989@gmail.com>
2023-01-15 15:26:21 -0500

    add routes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require(`express`)
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,6 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('*', (req, res) => res.sendFile(`index.html`));
+app.get(`/notes`, (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
 
-app.get('/', (req, res) => res.sendFile('index.html'));
-app.listen(PORT, () => console.log(`listening o port ${PORT}`))
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+  });
+app.post(`api/notes`, (req, res) => req.json(res) )
+
+app.listen(PORT, () => console.log(`listening to port ${PORT}`))
