ec6041e0c88097df588f9d0c52a60613b43377ca
Ivan Escobar <ivnescbr1989@gmail.com>
2023-01-19 18:12:26 -0500

    add fs to save notees array in fs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // dependencies
 const express = require(`express`)
 const path = require('path');
+const fs = require(`fs`)
 const uuid = require('uuid-random');
-let notes = require(`./db/db.json`) //access json file in directory
+let notes = require(`./db/db.json`); //access json file in directory
+const { json } = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 //middleware that parses incoming req. w/ json payload
@@ -21,18 +23,20 @@ app.post(`/api/notes`, (req, res) => {
         id: uuid()
     }
     notes.push(newNote)
+    fs.writeFile(`./db/db.json`, JSON.stringify(notes), (err) => err ? console.error(err) : console.log(`db updated`))
     res.json(notes)
 })
 app.delete(`/api/notes/:id`, (req, res) => {
     notes.forEach((note) => {
         if (note.id === req.params.id) {
             notes = notes.filter(note => note.id !== req.params.id)
+            fs.writeFile(`./db/db.json`, JSON.stringify(notes), (err) => err ? console.error(err) : console.log(`db updated`))
             res.json(notes)
         }
     })
 })
 //page routes
 app.get(`/notes`, (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-app.get(`*`, (req, res) => res.sendFile(`index.html`));
+app.get(`*`, (req, res) => res.sendFile(path.join(__dirname, `/public/index.html`)));
 
 app.listen(PORT, () => console.log(`${uuid()}: listening to port http://localhost${PORT}`));
